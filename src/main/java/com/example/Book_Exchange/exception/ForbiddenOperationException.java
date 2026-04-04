61db4b2ba1d5dd8b4b7ba53b9aee16217255c34c
Arman-2107046 <armanr.rafi@gmail.com>
2026-04-04 21:32:24 +0600

    fix build test failure in phase7

--- src/main/java/com/example/Book_Exchange/exception/ForbiddenOperationException.java
+++
@@ -1,7 +0,0 @@
-package com.example.Book_Exchange.exception;
-
-public class ForbiddenOperationException extends RuntimeException {
-    public ForbiddenOperationException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/Book_Exchange/exception/ResourceConflictException.java
+++
@@ -1,7 +0,0 @@
-package com.example.Book_Exchange.exception;
-
-public class ResourceConflictException extends RuntimeException {
-    public ResourceConflictException(String message) {
-        super(message);
-    }
-}
